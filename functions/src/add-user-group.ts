49950ae0a30ea73249a93745c8f0c970eabb7646
Husso1997 <hussain1961@hotmail.com>
2019-05-01 09:37:37 +0200

    Added function

--- functions/src/add-user-group.ts
+++ functions/src/add-user-group.ts
@@ -3,28 +3,34 @@ import * as admin from 'firebase-admin';
 
 
 
-exports.addUserToGroup  = functions.https.onRequest((req, res) => {
+exports.addUserToGroup  = functions.https.onRequest(async (req, res) => {
 
     const data = req.body;
     const collection = data.collection;
     const doc = data.doc;
-    const users = [];
-
-
-    admin.firestore().collection(collection).doc(doc).get().then( async documentData => {
-        users = await documentData.data()
-    }).catch(error => {
-        console.log(error);
-    });
-    users.push(data.user);
-
-    admin.firestore().collection(collection).doc(doc).set({users: users
-        },
-        {merge: true}
-    ).catch(error => {
-        console.log(error)
-    });
-
-    res.send('hello');
-
+    const users = data.users;
+    // Check if collection, doc are not empty and that user list is defined.
+    if(collection && doc && users)
+    {
+        // Adding the newly added user.
+        users.push(data.user);
+        try {
+            admin.firestore().collection(collection).doc(doc).set({
+                    users: users
+                },
+                {merge: true}
+            ).catch(error => {
+                console.log(error);
+                throw error;
+            });
+            res.send('User was successfully added');
+        }
+        catch(error) {
+            res.send('Something unexpected happened. Error.')
+        }
+    }
+    else
+    {
+        res.send('Missing parameters.')
+    }
 });
