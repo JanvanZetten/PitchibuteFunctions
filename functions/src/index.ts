0b73f945a2827640df14872ee2527fbfc5b497fc
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2019-05-01 08:43:28 +0200

    Added example function getting all files downloadURL

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as admin from 'firebase-admin';
 import * as uploadFile from './upload-file';
+import * as testDownloadFile from './test-download-file';
 
-admin.initializeApp();
+// Fetch the service account key JSON file contents
+const serviceAccount = require("../pitchibute-5171e0a0103b.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "pitchibute.appspot.com"
+});
 
 module.exports = {
+    ...testDownloadFile,
     ...uploadFile
 }
