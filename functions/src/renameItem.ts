158064edf3af1ae29f92cd3491888978653a663e
Husso1997 <hussain1961@hotmail.com>
2019-05-15 13:07:50 +0200

    cors and documentation

--- functions/src/renameItem.ts
+++ functions/src/renameItem.ts
@@ -3,62 +3,65 @@ import * as functions from 'firebase-functions';
 import {Authorization} from "./authorization";
 import {Helper} from "./helpers/helper";
 import {CustomError} from "./custom-error";
+import * as corsModule from 'cors';
 
-exports.renameItem = functions.https.onRequest(async (req, res) => {
-    const helper = new Helper();
-    const auth = new Authorization();
-    const reqData = req.body;
+const cors = corsModule({
+    origin: true, exposedHeaders: ['Content-Type', 'Content-Disposition', 'Content-Length']
+});
+exports.renameItem = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+        const helper = new Helper();
+        const auth = new Authorization();
+        const reqData = req.body;
 
-    const newName = reqData.name;
-    const doc = reqData.doc;
+        const newName = reqData.name;
+        const doc = reqData.doc;
 
-    const collection = reqData.collection;
+        const collection = reqData.collection;
 
-    let userRequestUid: string;
+        let userRequestUid: string;
 
-    if (newName && collection && doc) {
-        try {
-            auth.validateFirebaseIdToken(req, res);
-            const collectionArray = collection.split('/');
+        if (newName && collection && doc) {
+            try {
+                const tokenBearer = auth.validateFirebaseIdToken(req);
+                const collectionArray = collection.split('/');
 
-            const tokenBearer = req.headers['authorization'];
-            // @ts-ignore
-            await auth.verifyToken(tokenBearer).then(token => {
-                userRequestUid = token.uid;
-                // @ts-ignore
-            }).catch(error => {
-                throw new CustomError('You are not authorized', 403);
-            });
-            await helper.getDocument(collectionArray[1], collectionArray[2])
-                .then(snapShot => {
+                await auth.verifyToken(tokenBearer).then(token => {
+                    userRequestUid = token.uid;
+                }).catch(error => {
+                    throw new CustomError('You are not authorized', 403);
+                });
+                await helper.getDocument(collectionArray[1], collectionArray[2])
+                    .then(snapShot => {
+                        const data = snapShot.data();
+                        // @ts-ignore
+                        if (!helper.apartOfGroup(data.users, userRequestUid)) {
+                            throw new CustomError('You are not authorized, not part of the group', 403);
+                        }
+                    }).catch(error => {
+                        throw error
+                    });
+
+                await helper.getDocument(collection, doc).then(snapShot => {
                     const data = snapShot.data();
                     // @ts-ignore
-                    if (!helper.apartOfGroup(data.users, userRequestUid)) {
-                        throw new CustomError('You are not authorized, not part of the group', 403);
-                    }
+                    helper.nameChecker(data.name, newName);
                 }).catch(error => {
                     throw error
                 });
 
-            await helper.getDocument(collection, doc).then(snapShot => {
-                const data = snapShot.data();
-                // @ts-ignore
-                helper.nameChecker(data.name, newName);
-            }).catch(error => {
-                throw error
-            });
+                helper.renameItem(collection, doc, newName);
+                res.send('Changed name of item')
 
-            helper.renameItem(collection, doc, newName);
-            res.send('Changed name of item')
-
-        } catch (error) {
-            if (error instanceof CustomError) {
-                res.status(error.errorStatus).send(error.message);
-            } else {
-                res.send('Something unexpected happened. Contact the administrator team')
+            } catch (error) {
+                if (error instanceof CustomError) {
+                    res.status(error.errorStatus).send(error.message);
+                } else {
+                    res.send('Something unexpected happened. Contact the administrator team')
+                }
             }
+        } else {
+            res.send('Missing parameters.')
         }
-    } else {
-        res.send('Missing parameters.')
-    }
+    })
 });
