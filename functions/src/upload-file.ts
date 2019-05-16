743f1a8562779c2c0541b4ddca92231f2810be60
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2019-05-16 11:46:25 +0200

    Authorized Upload

--- functions/src/upload-file.ts
+++ functions/src/upload-file.ts
@@ -10,7 +10,74 @@ enum type {
  * When files are uploaded to firebase storage, the metadata is added to a document in the files collection.
  */
 exports.uploadfile =
-    functions.storage.object().onFinalize((object) => {
+    functions.storage.object().onFinalize(async (object, context) => {
+        console.log('Object: ' + JSON.stringify(object));
+
+        let splitPath = new Array<string>();
+        let shouldDestroyFile = false;
+        // Checks if Authentication is set. If not deletes the file.
+        // Checks if user is allowed to add file to group.
+        if (object && object.metadata && object.metadata.token && object.metadata.path) {
+            let decodedUserUid = '';
+
+            await admin.auth().verifyIdToken(object.metadata.token).then(token => {
+                decodedUserUid = token.uid;
+            }).catch(() => {
+                console.error('Invalid token: ' + decodedUserUid);
+                shouldDestroyFile = true;
+                });
+
+            if (!shouldDestroyFile) {
+                console.log('Decoded User Id: ' + decodedUserUid);
+
+                splitPath = object.metadata.path.split('/').filter(Boolean);
+
+                // Calculate root item path.
+                let rootItemPath = '';
+                if (splitPath.length > 0) {
+                    rootItemPath = splitPath[0];
+                }
+
+                if (rootItemPath !== '') {
+                    console.log('Root item path: ' + rootItemPath);
+                    await admin.firestore().collection('items').doc(rootItemPath).get()
+                        .then(doc => {
+                            if (doc && doc.exists && doc.data()) {
+                                const docData = doc.data();
+                                if (docData && docData.users) {
+                                    console.log('Document users: ', docData.users);
+                                    if (docData.users.indexOf(decodedUserUid) < 0) {
+                                        console.error('Unauthorized, User UID: ' + decodedUserUid);
+                                        shouldDestroyFile = true;
+                                    }
+                                } else {
+                                    console.error('Unauthorized, User UID: ' + decodedUserUid);
+                                    shouldDestroyFile = true;
+                                }
+                            } else {
+                                console.error('Unauthorized, User UID: ' + decodedUserUid);
+                                shouldDestroyFile = true;
+                            }
+                        })
+                        .catch(err => {
+                            console.error(err + ', User UID: ' + decodedUserUid);
+                            shouldDestroyFile = true;
+                        });
+                } else {
+                    console.error('Unauthorized, User UID: ' + decodedUserUid);
+                    shouldDestroyFile = true;
+                }
+            }
+        } else {
+            console.error('Missing data: ' + JSON.stringify(object));
+            shouldDestroyFile = true;
+        }
+
+        console.log('Should destroy file: ' + shouldDestroyFile);
+        if (shouldDestroyFile) {
+            return destroyFile(object);
+        }
+
         return new Promise((resolve, reject) => {
             if (object && object.name && object.metadata) {
 
@@ -22,14 +89,11 @@ exports.uploadfile =
                     size: object.size
                 };
 
-                const path = object.metadata.path
-                const docIds = path.split('/')
-
                 const ITEMS = 'items'
 
                 let collection = admin.firestore().collection(ITEMS)
 
-                docIds.forEach(docId => {
+                splitPath.forEach(docId => {
                     if (docId !== '') {
                         collection = collection.doc(docId).collection(ITEMS)
                     }
@@ -49,3 +113,18 @@ exports.uploadfile =
             }
         });
     });
+
+async function destroyFile(object: functions.storage.ObjectMetadata) {
+    if (object.name) {
+        await admin.storage().bucket().file(object.name).delete().then(() => {
+            console.log('Successfully deleted unauthorized file upload: ' + object.name);
+        }).catch(err => {
+            console.error('Failed deleting unauthorized file upload: ' + object.name);
+        });
+    } else {
+        console.error('Failed deleting unauthorized file upload, missing name: ' + JSON.stringify(object));
+    }
+    return new Promise((resolve, reject) => {
+        reject('Unauthorized action succeeded');
+    });
+}
