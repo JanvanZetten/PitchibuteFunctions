acb43e3ac33be3b67a8b51fee587bdabaee355a6
Applemelon <32298607+Applemelon@users.noreply.github.com>
2019-05-21 09:39:41 +0200

    Unnecessary import

--- functions/src/delete-item.ts
+++ functions/src/delete-item.ts
@@ -1,5 +1,4 @@
 import * as functions from 'firebase-functions';
-//import * as admin from 'firebase-admin';
 import {Authorization} from "./authorization";
 import {Helper} from "./helpers/helper";
 import {CustomError} from "./custom-error";
